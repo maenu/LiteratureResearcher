ad377d331be89814fce21ca220a7e8f30ab6020f
Manuel Leuenberger <leuenberger@inf.unibe.ch>
2017-10-12 13:41:02 +0200

    remove cleaning, shouldn't harm too much

--- src/main/java/ch/unibe/scg/pdflinker/Linker.java
+++ src/main/java/ch/unibe/scg/pdflinker/Linker.java
@@ -6,11 +6,8 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
@@ -24,36 +21,11 @@ public class Linker {
 
 	public void link(String id, File in, File out, List<String> keys) throws InvalidPasswordException, IOException {
 		try (PDDocument document = PDDocument.load(in)) {
-			List<Reference> references = (new ParagraphStripper()).getParagraphs(document).stream()
+			(new ParagraphStripper()).getParagraphs(document).stream()
 					.map(paragraph -> this.asReference(paragraph, keys)).filter(Optional::isPresent).map(Optional::get)
-					.collect(Collectors.toList());
-			this.clean(references).forEach(reference -> this.addHyperLink(id, document, reference));
+					.forEach(reference -> this.addHyperLink(id, document, reference));
 			document.save(out);
 		}
-	}
-
-	private List<Reference> clean(List<Reference> references) {
-		List<Integer> indexes = new ArrayList<>(new TreeSet<>(references.stream().map(Reference::getParagraph)
-				.map(Paragraph::getPageIndex).collect(Collectors.toList())));
-		int longestLength = 1;
-		int end = 0;
-		int length = 1;
-		for (int i = 1; i < indexes.size(); i = i + 1) {
-			if (indexes.get(i) != indexes.get(i - 1)) {
-				length = 1;
-			} else {
-				length = length + 1;
-			}
-			// precedence for tail
-			if (length >= longestLength) {
-				longestLength = length;
-				end = i;
-			}
-		}
-		final List<Integer> bibliographyPageIndexes = indexes.subList(end - longestLength, end + 1);
-		return references.stream()
-				.filter(reference -> bibliographyPageIndexes.contains(reference.getParagraph().getPageIndex()))
-				.collect(Collectors.toList());
 	}
 
 	private Optional<Reference> asReference(Paragraph paragraph, List<String> keys) {
